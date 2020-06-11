f9c02735eae68afed97bfdc249b04bea8ec956ff
Emirhan Akman <32221041+EmirhaanAkman@users.noreply.github.com>
2020-06-11 16:06:14 +0300

    Add files via upload

--- 180401068.py
+++ 180401068.py
@@ -1,190 +1,151 @@
-#!/usr/bin/env python
-# coding: utf-8
+#Emirhan Akman 180401068
 
-# In[1]:
+def matrisOlus(matrisHesapla):
+    list = matrisHesapla.copy()
+    satir = len(matrisHesapla)
+    sutun = len(matrisHesapla[0])
+
+    for i in range(satir-1):
+        for j in range(satir-1-k):
+            carp = list [j][i]/list[j+1][i]
+            for k in range(sutun):
+                list [j][k] += -carp * list[j+1][k]
+
+    for a in range(satir-1,0,-1):
+        for s in range(satir-1,satir-1-a,-1):
+            carp= list[s][a]/list[s-1]/[a]
+            for d in range(sutun):
+                list[s][d] += -carp* list[s-1][d]
+    result = []
+    for l in range(satir-1,-1,-1):
+        x= list[l][satir]/list[l][satir-l-1]
+        result.append(x)
+    return result
+
+with open("veriler.txt","r",encoding='utf-8') as file:
+    liste = []
+    for i in file.read().split():
+        liste.append(int(i))
+
+def boyut(liste):
+    return len(liste)
+n = boyut(liste)
+
+def yTop(liste):
+    x=sum(liste)
+    return x
+yitoplam= yTop(liste)
+
+def xiTop(n):
+    xikiTop= []
+    for k in range(1,13,1):
+        xiki =0
+        for l in range(n):
+            xiki += (i+1)**j
+        xikiTop.append(xiki)
+    xikiTop.insert(0,n)
+    return xikiTop
+
+def xDereceTop(n,liste,yTop):
+    xDerece_top = []
+    for i in range(1,7,1):
+        value = 0
+        for j in range(n):
+            value += (j+1)**i*liste[j]
+        xDerece_top.append(value)
+    xDerece_top.insert(0,yTop)
+    return xDerece_top
 
 
-#Emirhan Akman
-#180401068
-import math
-veriler = []
-korelasyonlar = []
+def aValues(n,liste,m=8):
+    result= []
+    xikiTop=xiTop()
+    y=yTop(liste)
+    xDerece_top = xDereceTop(n,liste,y)
+    for x in range(2,m,1):
+        newListe= []
+        for i in range(x):
+            newListe.append([])
+            for j in range(x):
+                newListe[i].append(xikiTop[j+i])
+            newListe[i].append(xDerece_top[i])
+            if i == x-1:
+                result.append(matrisOlus(newListe))
+                newListe.clean()
+    return result
+Adeger = aValues(n,liste)
 
-
-# In[5]:
-
-
-def korelasyon(mSonuc,ilk,son):
-    n = son - ilk
-    yi = 0
-    for i in range(ilk,son):
-        yi+=veriler[i]
-    y_ust = yi/n  
-    Sr = 0
-    for j in range(ilk,son):
-        Sr = (mSonuc[j-ilk] - veriler[j]) ** 2 + Sr
-    St = 0
-    for k in range(ilk,son):
-        St = St + (veriler[k] - y_ust)**2
-    rsqrt=  abs((St-Sr)/St)
-    r = math.sqrt(rsqrt)
+def srHesap(x,liste,n,yTop):
+    sr=0
+    st=0
+    y=yTop/n
+    boyut= len(x)
+    for i in range(n):
+        temp=0
+        for j in range(boyut):
+            if j==0:
+                temp += x[j]
+            else:
+                temp += x[j]*(i+1)**j
+        sr += (liste[i]-temp)**2
+        st += (liste[i]-y)**2
+    r = ((st - sr) / st) ** (1 / 2)
     return r
+corValue = []
+for i in Adeger:
+    r = srHesap(i,list,n,yitoplam)
+    corValue.append(r)
 
+def bestCorrelation(liste):
+    theBiggest = max(liste)
+    i=1
+    while theBiggest!=liste[i-1]:
+        i+=1
+    return i,theBiggest
 
-# In[2]:
+index,bestCor= bestCorrelation(corValue)
+print("En düşük hatayı veren polinom derecesi:",index)
+print("Kolerasyon değeri:",bestCor)
+print('\n')
+polinom = Adeger[index-1]
 
+def fonk(x,pol=polinom):
+    aKatsayisi = pol
+    topVeri=0
+    for i in range(len(aKatsayisi)):
+        topVeri += aKatsayisi[i]* (x ** i)
+    return topVeri
 
-def gaussYontem(A):
-    boyut = len(A)
-    for i in range(0, boyut):
-        maxSutun = abs(A[i][i])
-        maxSatir = i
-        for j in range(i+1, boyut):
-            if abs(A[j][i]) > maxSutun:
-                maxSutun = abs(A[j][i])
-                maxSatir = j
-        for k in range(i, boyut+1):
-            temp = A[maxSatir][k]
-            A[maxSatir][k] = A[i][k]
-            A[i][k] = temp
-            
-        for l in range(i + 1, boyut):
-            c = -A[l][i] / A[i][i]
-            for j in range(i, boyut+1):
-                if i == j:
-                    A[l][j] = 0
-                else:
-                    A[l][j] += c * A[i][j]
+def polinomlu(n):
+    #numaramın son rakamı 8, 180401068
+    a= 8
+    b=ndeltax = 0.001
+    integral=0
+    boyut = int((b-a)/deltax)
+    for i in range(boyut):
+        integral += deltax*(fonk(a)+fonk(a+deltax))/2
+        a+= deltax
+    print("Polinomlu sonuc:",integral)
 
-    matris = [0 for i in range(boyut)]
-    for m in range(boyut-1, -1, -1):
-        matris[m] = A[m][boyut] / A[m][m]
-        for n in range(i-1, -1, -1):
-            A[n][boyut] -= A[n][m] * matris[m]
-    return matris
+def polinomsuz(n,liste):
+    a=8
+    b=n
+    integral =0
+    for i in range(a-1,b-1):
+        integral += (liste[i]+ liste[i+1])/2
+    print("Polinom olmadan sonuc",integral)
 
-
-# In[3]:
-
-
-
-def findData(ilk,son):     
-    dizi=[]
-    n = son-ilk
-    for degree in range(1,7):
-        xDegeri = []
-        for i in range(n):
-            xDegeri.appson(i+1)   
-        matris = [[0 for i in range(degree+1)] for j in range(degree+1)]
-        boyut = len(matris)
-    
-        for i in range(boyut):
-            for j in range(boyut):
-                xToplamlari = 0
-                for k in range(n):
-                    matris[0][0] = len(xDegeri) 
-                    xToplamlari=xDegeri[k] ** (i+j) + xToplamlari  
-                    matris[i][j] = xToplamlari
-                
-        
-        
-       
-        xyToplami = []
-        for i in range(boyut):
-            toplam=0
-            for j in range(ilk,son):
-                toplam = toplam + (veriler[j] * (xDegeri[j-ilk] ** i))    
-            xyToplami.appson(toplam)
-        
-      
-        k = 0        
-        for i in matris:
-            i.appson(xyToplami[k])
-            k = k+1
-    
-    
-            
-        katSayilar = gaussYontem(matris)
-        
-        mSonuc = []
-        for i in range(n):
-            toplam = 0
-            for j in range(len(katSayilar)):
-                toplam = toplam + katSayilar[j] * ((i+1) ** j)
-                if j == degree:
-                    mSonuc.appson(int(toplam))
-        r = korelasyon(mSonuc,ilk,son) 
-        dizi.appson(r)
-        yaz(mSonuc, katSayilar, ilk, son, degree, r) 
-    best = 100
-    index=0
-    for i in range(len(dizi)):
-        temp = abs(1-dizi[i])
-        if temp < best:
-            best = temp
-            index = i+1
-
-
-# In[4]:
-
-
-def verOku():
-    ver = open('Veriler.txt','r')
-    for satir in ver:
-        veriler.appson(int(satir))
-    ver.close()
-
-
-# In[6]:
-
-
-def yaz(mSonuc, katSayilar, ilk, son, degree, r):
-    ver = open('sonuc.txt','a+')
-    ver.write('------- '+str(ilk)+' - '+ str(son-1)+'(dahil) ------------- Indexdeki aralıklar için  '+str(degree)+'.  dereceden yaklaşım \n')
-    ver.write('Korelasyon degerleri:  '+str(r)+'\n')
-    ver.write(' Katsayilar: ')
-    ver.write('\n')
-    for i in katSayilar:
-        ver.write(str(i))
-        ver.write('\t')
-    ver.write('\n')
-    ver.write('Ham veri , Bizim Sonuclarimiz: ')
-    ver.write('\n')
-    for i in range(ilk, son):
-        ver.write(str(veriler[i]))
-        ver.write('\t')
-        ver.write(str(mSonuc[i-ilk]))
-        ver.write('\n')
-    ver.close()
-
-
-# In[7]:
-
-
-ver = open('sonuc.txt', 'a+')
-ver.write(str(ilk)+'-'+str(son-1)+' arası en iyi korelasyona sahip derece: '+str(index)+'\n')
-ver.write('Korelasyon degeri:  '+str(dizi[index-1])+'\n')
-ver.write('------------------------------------')
-ver.write('\n')
-ver.close()
-
-
-
-
-
-verOku()    
-findData(0, len(veriler))  
-
-bas = 0
-bitis = 10   
-while(bitis < len(veriler)): 
-   findData (bas,son)
-   bas = bas+10
-   bitis = bitis+10
-
-
-# In[ ]:
-
+def yorum():
+    with open("180401068_yorum.txt","w",encoding="utf-8")as yorum:
+        yorum.write("Emirhan Akman 180401068\n")
+        yorum.write("Polinomlu ve polinomsuz hesaplanmış integraller farklı sonuclar verir. \n")
+        yorum.write("Bunun nedeni delta x için belirlediğimiz değerin uzunluğundandır.\n")
+        yorum.write("Delta x in degeri , hesaplanacak grafigin kac adet alana bölünecegini isaret eder.\n")
+        yorum.write("Delta x'i ne kadar küçük alırsak hesaplanması gereken alan sayısı o kadar artacagından\n")
+        yorum.write("Grafik ve alan arasubdaki bosluk ve grafik dısına tasmalar azalacaktır.\n")
+        yorum.write("Boylece hesapladıgımız deger, reel degerine olabildigince cok yaklasır.\n")
+        yorum.write("Polinomsuz hesaplamada ise grafigi böldügümüz alanı kendimiz belirlemedigimiz icin,\n")
+        yorum.write("boldugümüz alanlar ile grafik arasında tasmalar olusur, böylece hata artar\n")
 
 
 
